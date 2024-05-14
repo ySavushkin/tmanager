18a0836dd6f3ef58496702a06012ebb85ed32c46
ySavushkin <144417407+ySavushkin@users.noreply.github.com>
2024-05-14 22:57:26 +0200

    initialize spring

---
+++ src/test/java/mainPackage/tmanager/TmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package mainPackage.tmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
