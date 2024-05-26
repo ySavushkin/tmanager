80bb99378c1b00982e776ff93feffca8855d7668
ySavushkin <elissssey@icloud.com>
2024-05-27 00:42:03 +0200

    swagger add

--- src/main/java/mainPackage/tmanager/config/SwaggerConfig.java
+++ src/main/java/mainPackage/tmanager/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package mainPackage.tmanager.configs;
+package mainPackage.tmanager.config;
 
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
@@ -10,11 +10,12 @@ import io.swagger.jaxrs.config.BeanConfig;
 
 public class SwaggerConfig extends Application {
     public SwaggerConfig() {
+        // Swagger configuration
         BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion("1.0.0");
         beanConfig.setBasePath("/api");
 
-        beanConfig.setResourcePackage("mainPackage.tmanager.controllers");
+        beanConfig.setResourcePackage("mainPackage.tmanager.controllers");// Configuring the base package for controller scanning
 
         beanConfig.setResourcePackage("org.jazzteam");
         beanConfig.setScan(true);
@@ -22,6 +23,7 @@ public class SwaggerConfig extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
+        // Connecting Swagger
         Set<Class<?>> resources = new HashSet<>();
         resources.add(ApiListingResource.class);
         resources.add(SwaggerSerializers.class);
